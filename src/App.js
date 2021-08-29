f2d5fa0bbe62347875c21c7ede53fa09e1881abf
Vivek P A <vivekpa70@gmail.com>
2021-08-29 18:47:14 +0530

    Add. Logic to 'add' and 'view' Todo tasks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
-
+import {TodoItem} from './TodoItem'
+import React, { useState, useEffect } from 'react';
 function App() {
+    const [todos, setTodos] = useState([]);
+    function handleSubmit(e) {
+        const {todoItem} = e.target.elements
+        let todoTask = todoItem.value
+        setTodos([...todos, todoTask])
+        e.preventDefault()
+        e.target.reset()
+    }
+
+    // Debugging
+    useEffect(() => {
+        console.log(todos)
+      }, [todos]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h2 className ="text-5xl"> Hello World!</h2>
-      </header>
+      <form onSubmit={handleSubmit}>
+       <input type="text" id="todoItem"/>
+       <input type="submit" value="Add Todo" />
+    </form>
+    {todos.map((todo, index) => (
+          <TodoItem todoItem = {todo} key = {index}/>
+      ))}
+
     </div>
+
   );
 }
 
---
+++ src/TodoItem.js
@@ -0,0 +1,6 @@
+const TodoItem = (props) =>{
+
+    return <h3  className = "border-2 border-indigo-600">{props.todoItem}</h3>
+}
+
+export {TodoItem};
